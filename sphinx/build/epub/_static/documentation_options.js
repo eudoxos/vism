3900163bcc89271935f4ffc0a598257d95bb3433
eudoxos <github@doxos.eu>
2023-03-02 09:37:00 +0000

    Deploying to gh-pages from @ eudoxos/vism@3a937c3bae4be996d919175b23e5e449715401e9 🚀

--- sphinx/build/epub/_static/documentation_options.js
+++ sphinx/build/epub/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'c41fd32',
+    VERSION: '3a937c3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'epub',
